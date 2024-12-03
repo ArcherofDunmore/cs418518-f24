dc0894ded67275882472c5a2f7d25a5f1a6debcc
Gavin <gclen001@odu.edu>
2024-12-03 12:49:09 -0500

    forgot to re-add useEffect on changeInfo and changePassword pages

--- Project/client/src/components/changePassword.jsx
+++ Project/client/src/components/changePassword.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 
